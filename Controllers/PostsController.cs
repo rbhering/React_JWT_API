a4d2d9a7d8c1180e71517aeeafea77c4b8e9dcd0
rbhering <77896350+rbhering@users.noreply.github.com>
2025-03-01 13:44:17 -0300

    Add project files.

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using api.Models.Entities;
+using api.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostsController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;   
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _postRepository.Get());
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _postRepository.GetPostById(id));
+        }
+
+        [HttpGet("PostByUser/{userId}")]
+        public async Task<IActionResult> GetPostByUser(int userId)
+        {
+            return Ok(await _postRepository.GetPostsByUser(userId));
+        }
+
+
+    }
+}
---
+++ Repositories/PostRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Post>> Get()
+        {
+            return await _context.Post.ToListAsync();
+        }
+
+        public async Task<Post> GetPostById(int id)
+        {
+            return await _context.Post.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Post>> GetPostsByUser(int userId)
+        {
+            return await _context.Post.Where(x => x.UserId == userId).ToListAsync();
+        }
+    }
+}
